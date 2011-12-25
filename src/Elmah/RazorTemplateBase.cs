a2785a2d596018534f167e5c44ea3805bb67d7f9
Atif Aziz <code@raboof.com>
2011-12-25 22:27:24 +0100

    Breaking out HtmlString and WebTemplateBase into own files

--- src/Elmah/RazorTemplateBase.cs
+++ src/Elmah/RazorTemplateBase.cs
@@ -1,93 +1,29 @@
+#region License, Terms and Author(s)
+//
+// ELMAH - Error Logging Modules and Handlers for ASP.NET
+// Copyright (c) 2004-9 Atif Aziz. All rights reserved.
+//
+//  Author(s):
+//
+//      Atif Aziz, http://www.raboof.com
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+#endregion
+
 namespace Elmah
 {
-    using System;
     using System.Text;
-    using System.Web;
-
-    // http://msdn.microsoft.com/en-us/library/system.web.ihtmlstring.aspx
-
-    interface IHtmlString
-    {
-        string ToHtmlString();
-    }
-
-    class HtmlString : IHtmlString
-    {
-        private readonly string _html;
-
-        public HtmlString(string html)
-        {
-            _html = html;
-        }
-
-        public string ToHtmlString() { return _html; }
-
-        public override string ToString() { return ToHtmlString(); }
-    }
-
-    class WebTemplateBase : RazorTemplateBase, IHttpHandler
-    {
-        public HttpContextBase Context { get; private set; }
-        public HttpApplicationStateBase Application { get { return Context.Application; } }
-        public HttpResponseBase Response { get { return Context.Response; } }
-        public HttpRequestBase Request { get { return Context.Request; } }
-        public HttpServerUtilityBase Server { get { return Context.Server; } }
-        public HttpSessionStateBase Session { get { return Context.Session; } }
-
-        public IHtmlString Html(string html)
-        {
-            return new HtmlString(html);
-        }
-
-        public string AttributeEncode(string text)
-        {
-            return string.IsNullOrEmpty(text)
-                 ? string.Empty
-                 : HttpUtility.HtmlAttributeEncode(text);
-        }
-
-        public string Encode(string text)
-        {
-            return string.IsNullOrEmpty(text) 
-                 ? string.Empty 
-                 : Server.HtmlEncode(text);
-        }
-
-        public override void Write(object value)
-        {
-            if (value == null)
-                return;
-            var html = value as IHtmlString;
-            base.Write(html != null ? html.ToHtmlString() : Encode(value.ToString()));
-        }
-
-        public override void Execute()
-        {
-            if (Context == null)
-                throw new InvalidOperationException("The Context property has not been initialzed with an instance.");
-            base.Execute();
-        }
-
-        void IHttpHandler.ProcessRequest(HttpContext context)
-        {
-            var oldContext = Context;
-
-            try
-            {
-                Context = new HttpContextWrapper(context);
-                context.Response.Write(TransformText());
-            }
-            finally
-            {
-                Context = oldContext;
-            }
-        }
-
-        bool IHttpHandler.IsReusable
-        {
-            get { return false; }
-        }
-    }
 
     class RazorTemplateBase
     {
---
+++ src/Elmah/WebTemplateBase.cs
@@ -0,0 +1,73 @@
+namespace Elmah
+{
+    #region Imports
+
+    using System;
+    using System.Web;
+
+    #endregion
+
+    class WebTemplateBase : RazorTemplateBase, IHttpHandler
+    {
+        public HttpContextBase Context { get; private set; }
+        public HttpApplicationStateBase Application { get { return Context.Application; } }
+        public HttpResponseBase Response { get { return Context.Response; } }
+        public HttpRequestBase Request { get { return Context.Request; } }
+        public HttpServerUtilityBase Server { get { return Context.Server; } }
+        public HttpSessionStateBase Session { get { return Context.Session; } }
+
+        public IHtmlString Html(string html)
+        {
+            return new HtmlString(html);
+        }
+
+        public string AttributeEncode(string text)
+        {
+            return string.IsNullOrEmpty(text)
+                       ? string.Empty
+                       : HttpUtility.HtmlAttributeEncode(text);
+        }
+
+        public string Encode(string text)
+        {
+            return string.IsNullOrEmpty(text) 
+                       ? string.Empty 
+                       : Server.HtmlEncode(text);
+        }
+
+        public override void Write(object value)
+        {
+            if (value == null)
+                return;
+            var html = value as IHtmlString;
+            base.Write(html != null ? html.ToHtmlString() : Encode(value.ToString()));
+        }
+
+        public override void Execute()
+        {
+            if (Context == null)
+                throw new InvalidOperationException("The Context property has not been initialzed with an instance.");
+            base.Execute();
+        }
+
+        void IHttpHandler.ProcessRequest(HttpContext context)
+        {
+            var oldContext = Context;
+
+            try
+            {
+                Context = new HttpContextWrapper(context);
+                context.Response.Write(TransformText());
+            }
+            finally
+            {
+                Context = oldContext;
+            }
+        }
+
+        bool IHttpHandler.IsReusable
+        {
+            get { return false; }
+        }
+    }
+}
