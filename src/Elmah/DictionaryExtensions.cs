4f80d60a8ad8436b9944b8bdb41e0ce8b9f42688
Atif Aziz <code@raboof.com>
2011-12-27 21:49:37 +0100

    Using generic dictionary extensions from Mannex

--- src/Elmah/DictionaryExtensions.cs
+++ src/Elmah/DictionaryExtensions.cs
@@ -29,24 +29,11 @@ namespace Elmah
 
     using System;
     using System.Collections;
-    using System.Collections.Generic;
 
     #endregion
 
     static class DictionaryExtensions
     {
-        public static V Find<K, V>(this IDictionary<K, V> dict, K key)
-        {
-            return Find(dict, key, default(V));
-        }
-
-        public static V Find<K, V>(this IDictionary<K, V> dict, K key, V @default)
-        {
-            if (dict == null) throw new ArgumentNullException("dict");
-            V value;
-            return dict.TryGetValue(key, out value) ? value : @default;
-        }
-
         public static T Find<T>(this IDictionary dict, object key, T @default)
         {
             if (dict == null) throw new ArgumentNullException("dict");
