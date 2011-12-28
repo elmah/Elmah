2a9bfbd8c6b9f54158090c3f94b9b5ab6f7d281c
Atif Aziz <code@raboof.com>
2011-12-29 00:27:51 +0100

    Importing NameValueCollection extension methods from Mannex

--- src/Elmah/Mannex.cs
+++ src/Elmah/Mannex.cs
@@ -84,3 +84,105 @@ namespace Mannex.Collections.Generic
         }
     }
 }
+
+namespace Mannex.Collections.Specialized
+{
+    #region Imports
+
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.Specialized;
+    using System.Linq;
+    using Generic;
+
+    #endregion
+
+    /// <summary>
+    /// Extension methods for <see cref="NameValueCollection"/>.
+    /// </summary>
+
+    static partial class NameValueCollectionExtensions
+    {
+        /// <summary>
+        /// Create a <see cref="NameValueCollection"/> from a sequence of
+        /// <see cref="KeyValuePair{String,String}"/>.
+        /// </summary>
+
+        public static NameValueCollection ToNameValueCollection(
+            this IEnumerable<KeyValuePair<string, string>> source)
+        {
+            if (source == null) throw new ArgumentNullException("source");
+
+            var collection = CreateCollection(source as ICollection<KeyValuePair<string, string>>);
+            collection.Add(source);
+            return collection;
+        }
+
+        /// <summary>
+        /// Create a <see cref="NameValueCollection"/> from an <see cref="IEnumerable{T}"/>
+        /// given a function to select the name and value of each <typeparamref name="T"/>
+        /// in the source sequence.
+        /// </summary>
+
+        public static NameValueCollection ToNameValueCollection<T>(
+            this IEnumerable<T> source,
+            Func<T, string> nameSelector,
+            Func<T, IEnumerable<string>> valuesSelector)
+        {
+            if (source == null) throw new ArgumentNullException("source");
+            if (nameSelector == null) throw new ArgumentNullException("nameSelector");
+            if (valuesSelector == null) throw new ArgumentNullException("valuesSelector");
+
+            var collection = CreateCollection(source as ICollection<T>);
+            collection.Add(source, nameSelector, valuesSelector);
+            return collection;
+        }
+
+        /// <summary>
+        /// Adds items from an <see cref="IEnumerable{T}"/>,
+        /// given a function to select the name and value of each <typeparamref name="T"/>
+        /// in the source sequence.
+        /// </summary>
+
+        public static void Add<T>(
+            this NameValueCollection collection,
+            IEnumerable<T> source,
+            Func<T, string> nameSelector,
+            Func<T, IEnumerable<string>> valuesSelector)
+        {
+            if (collection == null) throw new ArgumentNullException("collection");
+            if (source == null) throw new ArgumentNullException("source");
+            if (nameSelector == null) throw new ArgumentNullException("nameSelector");
+            if (valuesSelector == null) throw new ArgumentNullException("valuesSelector");
+
+            var items = from item in source
+                        from value in valuesSelector(item)
+                        select nameSelector(item).AsKeyTo(value);
+
+            collection.Add(items);
+        }
+
+        /// <summary>
+        /// Adds items from a sequence of 
+        /// <see cref="KeyValuePair{String,String}"/>.
+        /// </summary>
+
+        public static void Add(
+            this NameValueCollection collection,
+            IEnumerable<KeyValuePair<string, string>> source)
+        {
+            if (collection == null) throw new ArgumentNullException("collection");
+            if (source == null) throw new ArgumentNullException("source");
+
+            foreach (var item in source)
+                collection.Add(item.Key, item.Value);
+        }
+
+        private static NameValueCollection CreateCollection<T>(ICollection<T> collection)
+        {
+            return collection != null
+                 ? new NameValueCollection(collection.Count)
+                 : new NameValueCollection();
+        }
+    }
+}
