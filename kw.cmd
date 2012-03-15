5e8d2aaab5542206e6466c1610950b2081a0c88b
Atif Aziz <code@raboof.com>
2012-03-15 22:42:02 +0100

    Expand author in Id keyword with user portion only

--- kw.cmd
+++ kw.cmd
@@ -7,7 +7,7 @@ goto :EOF
 :id
 setlocal
 echo %1
-for /f "usebackq tokens=*" %%i in (`hg parents %1 --template "%~nx1 {node|short} {date|isodate} {author}\n"`) do set x=%%i
+for /f "usebackq tokens=*" %%i in (`hg parents %1 --template "%~nx1 {node|short} {date|isodate} {author|user}\n"`) do set x=%%i
 call replace %1 \$Id(:.+?)?\$ "$Id: %x% $" > %temp%\%~nx1
 copy %temp%\%~nx1 %1 /y > nul
 goto :EOF
