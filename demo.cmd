b41f0046c51bdaeb530ec995e802f997d5edd796
Atif Aziz <code@raboof.com>
2012-05-13 18:46:20 +0200

    Updating demo.cmd from 1x repo

--- demo.cmd
+++ demo.cmd
@@ -38,11 +38,28 @@ if not "!DELAY_TEST!"=="test" (
 )
 pushd "%~dp0"
 
+set LIB_PATH_x64=lib\x64
+set BIN_PATH=bin\net-2.0\Release
 set DEMO_PATH=samples\Demo
+set DEMO_BIN_PATH=%DEMO_PATH%\bin
 set DEMO_PORT=54321
 set TOOLS_PATH=tools
 
-call build
+if exist "%SystemRoot%\Microsoft.NET\Framework\v2.0.50727" goto go
+
+echo The .NET Framework 2.0 does not appear to be installed on this 
+echo machine, which is required to run the demo Web site.
+set /p answer=Proceed anyway?
+if "%answer%"=="y" goto go
+if "%answer%"=="Y" goto go
+exit /b 1
+
+:go
+if not exist "%BIN_PATH%" call build 2.0
+if not exist "%DEMO_BIN_PATH%" md "%DEMO_BIN_PATH%"
+copy /y "%BIN_PATH%" "%DEMO_BIN_PATH%"
+
+if %PROCESSOR_ARCHITECTURE%==AMD64 copy /y "%LIB_PATH_x64%\System.Data.SQLite.DLL" "%DEMO_BIN_PATH%"
 
 set MAIL_PATH=%DEMO_PATH%\Mails
 if not exist "%MAIL_PATH%" md "%MAIL_PATH%"
