a3004d16135eb20f19811c5c87880e93cc281f99
Atif Aziz <code@raboof.com>
2012-06-19 23:55:43 +0200

    Using object initializer syntax for Error instantiation in OracleErrorLog

--- src/Elmah/OracleErrorLog.cs
+++ src/Elmah/OracleErrorLog.cs
@@ -426,16 +426,17 @@ namespace Elmah
                             var id = reader["ErrorId"].ToString();
                             var guid = new Guid(id);
 
-                            var error = new Error();
-
-                            error.ApplicationName = reader["Application"].ToString();
-                            error.HostName = reader["Host"].ToString();
-                            error.Type = reader["Type"].ToString();
-                            error.Source = reader["Source"].ToString();
-                            error.Message = reader["Message"].ToString();
-                            error.User = reader["UserName"].ToString();
-                            error.StatusCode = Convert.ToInt32(reader["StatusCode"]);
-                            error.Time = Convert.ToDateTime(reader["TimeUtc"]).ToLocalTime();
+                            var error = new Error
+                            {
+                                ApplicationName = reader["Application"].ToString(),
+                                HostName = reader["Host"].ToString(),
+                                Type = reader["Type"].ToString(),
+                                Source = reader["Source"].ToString(),
+                                Message = reader["Message"].ToString(),
+                                User = reader["UserName"].ToString(),
+                                StatusCode = Convert.ToInt32(reader["StatusCode"]),
+                                Time = Convert.ToDateTime(reader["TimeUtc"]).ToLocalTime()
+                            };
 
                             errorEntryList.Add(new ErrorLogEntry(this, guid.ToString(), error));
                         }
