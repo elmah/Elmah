7e83f10563de0e82863d2f9360a9414651060cf3
Atif Aziz <code@raboof.com>
2012-09-17 08:13:08 +0200

    Disabling building of projects targeting 4.5 if it's not installed

--- build.cmd
+++ build.cmd
@@ -30,5 +30,20 @@ if not exist "%MSBUILDEXE%" (
     echo solution.
     exit /b 1
 )
+for /f %%v in ('%MSBUILDEXE% /version /nologo') do for /f "delims=. tokens=1-4" %%i in ("%%v") do set vmaj=%%i& set vmin=%%j& set vbld=%%k& set vrev=%%l
+if not "%vmaj%.%vmin%.%vbld%"=="4.0.30319" (
+    echo There was a problem determining the version of your Microsoft Build Engine 
+    echo ^(MSBuild^) 4.0 installation.
+    exit /b 1
+)
+set build="%MSBUILDEXE%" Elmah.sln /p:DownloadNuGetExe=true
+if %vrev% lss 17929 (
+    echo ================================= WARNING! ================================
+    echo Projects targeting Microsoft .NET Framework 4.5 will be skipped as it does 
+    echo not appear to be installed on this machine.
+    echo ===========================================================================
+    echo.
+    set build=%build% /t:Elmah;Elmah_net-4_0;Elmah_Tests
+)
 set EnableNuGetPackageRestore=true
-for %%i in (debug release) do if exist "%MSBUILDEXE%" "%MSBUILDEXE%" Elmah.sln /p:Configuration=%%i /p:DownloadNuGetExe=true %*
+for %%i in (debug release) do %build% /p:Configuration=%%i %*
