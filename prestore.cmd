7abef316af17c3e78280dc9a59d941e33e0db326
Atif Aziz <code@raboof.com>
2013-10-14 21:30:43 +0200

    Package restore script fixes (possibly some due to PowerShell 4 compatibility issues)

--- prestore.cmd
+++ prestore.cmd
@@ -4,7 +4,7 @@ for %%i in (NuGet.exe) do set nuget_path=%%~dpnx$PATH:i
 if "%nuget_path%"=="" goto :nonuget
 set packages_path=%~dp0packages
 if exist "%packages_path%\repositories.config" (
-    for /f "usebackq delims=" %%p in (`PowerShell -C "Get-Content '%packages_path%\repositories.config' | Select-Xml //repository/@path | %%{$_.Node.Value}"`) do if errorlevel==0 call :restore "%packages_path%\%%p"
+    for /f "usebackq delims=" %%p in (`PowerShell -C "[xml](Get-Content '%packages_path%\repositories.config') | Select-Xml //repository/@path | %%{$_.Node.Value}"`) do if errorlevel==0 call :restore "%packages_path%\%%p"
 ) else (
     for /r %%d in (.) do if errorlevel==0 if exist %%d\packages.config call :restore "%%d"
 )
@@ -13,10 +13,8 @@ goto :EOF
 :restore
 setlocal
 echo Restoring packages for "%~1"
-cd "%~1" 
-"%nuget_path%" install -OutputDirectory "%packages_path%" ^
-                       -Source https://nuget.org/api/v2/ ^
-                       -Source P:\Development\NuGet
+cd "%~dp1" 
+"%nuget_path%" install -OutputDirectory "%packages_path%"
 goto :EOF
 
 :nonuget
