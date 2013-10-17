c62559cd8e2a9a0d4258093502467d35b889b86a
Atif Aziz <code@raboof.com>
2013-10-17 19:14:27 +0200

    Fixes in package restore script when repositories.config is missing

--- prestore.cmd
+++ prestore.cmd
@@ -6,15 +6,15 @@ set packages_path=%~dp0packages
 if exist "%packages_path%\repositories.config" (
     for /f "usebackq delims=" %%p in (`PowerShell -C "[xml](Get-Content '%packages_path%\repositories.config') | Select-Xml //repository/@path | %%{$_.Node.Value}"`) do call :restore "%packages_path%\%%p" || exit /b 1
 ) else (
-    for /r %%d in (.) do if exist %%d\packages.config call :restore "%%d" || exit /b 1
+    for /r %%d in (.) do if exist "%%~d\packages.config" call :restore "%%~d\packages.config" || exit /b 1
 )
 goto :EOF
 
 :restore
 setlocal
-echo Restoring packages for "%~1"
+echo Restoring packages for "%~dp1"
 cd "%~dp1" 
-"%nuget_path%" install -OutputDirectory "%packages_path%"
+"%nuget_path%" install %1 -OutputDirectory "%packages_path%"
 exit /b %errorlevel%
 
 :nonuget
