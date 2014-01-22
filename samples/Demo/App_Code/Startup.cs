e4df92b7e85f55974f853392664827f84fc0efbf
Atif Aziz <code@raboof.com>
2014-01-22 01:04:17 +0100

    Tabs to spaces

--- samples/Demo/App_Code/Startup.cs
+++ samples/Demo/App_Code/Startup.cs
@@ -3,8 +3,8 @@ using Elmah;
 
 static class Startup
 {
-	public static void Configuration(IAppBuilder app)
-	{
+    public static void Configuration(IAppBuilder app)
+    {
         app.UseElmahWeb();
-	}
+    }
 }
