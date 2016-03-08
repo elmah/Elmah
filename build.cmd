2a8621310e0ffe91ee55b954cb9e278ab5711584
Atif Aziz <code@raboof.com>
2016-03-08 08:49:45 +0100

    Build script fix to recognize MSBuild 12.0.40629.0

--- build.cmd
+++ build.cmd
@@ -30,26 +30,32 @@ goto :EOF
 :main
 set MSBUILDEXE=%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe
 if not exist "%MSBUILDEXE%" (
-    echo Microsoft Build Engine ^(MSBuild^) 4.0 does not appear to be 
-    echo installed on this machine, which is required to build the 
+    echo Microsoft Build Engine ^(MSBuild^) 4.0 does not appear to be
+    echo installed on this machine, which is required to build the
     echo solution.
     exit /b 1
 )
 for /f %%v in ('%MSBUILDEXE% /version /nologo') do for /f "delims=. tokens=1-4" %%i in ("%%v") do set vmaj=%%i& set vmin=%%j& set vbld=%%k& set vrev=%%l
-if not "%vmaj%.%vmin%.%vbld%"=="4.0.30319" (
-    echo There was a problem determining the version of your Microsoft Build Engine 
-    echo ^(MSBuild^) 4.0 installation.
-    exit /b 1
-)
+echo %vmaj% | findstr [0-9][0-9]* > nul || goto :unknown-msbuild-version
+echo %vmin% | findstr [0-9][0-9]* > nul || goto :unknown-msbuild-version
+echo %vbld% | findstr [0-9][0-9]* > nul || goto :unknown-msbuild-version
+echo %vrev% | findstr [0-9][0-9]* > nul || goto :unknown-msbuild-version
 set build="%MSBUILDEXE%" Elmah.sln
-if %vrev% lss 17929 (
-    echo ================================= WARNING! ================================
-    echo Projects targeting Microsoft .NET Framework 4.5 will be skipped as it does 
-    echo not appear to be installed on this machine.
-    echo ===========================================================================
-    echo.
-    set build=%build% /t:Elmah;Elmah_net-4_0;Elmah_Tests
+if "%vmaj%.%vmin%.%vbld%"=="4.0.30319" (
+    if %vrev% lss 17929 (
+        echo ================================= WARNING! ================================
+        echo Projects targeting Microsoft .NET Framework 4.5 will be skipped as it does
+        echo not appear to be installed on this machine.
+        echo ===========================================================================
+        echo.
+        set build=%build% /t:Elmah;Elmah_net-4_0;Elmah_Tests
+    )
 )
 call prestore ^
  && (for %%v in (3.5 4.0 4.5) do for %%c in (Debug Release) do %build% "/p:Configuration=NETFX %%v %%c;AspNetConfiguration=%%c" /v:m %* || exit /b 1)
 goto :EOF
+
+:unknown-msbuild-version
+echo There was a problem determining the version of your Microsoft Build Engine
+echo ^(MSBuild^) 4.0 installation.
+exit /b 1
