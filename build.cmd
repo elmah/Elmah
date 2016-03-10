f590cecd3c99fb22447ea97f8f22671c3caf3899
Atif Aziz <code@raboof.com>
2016-03-10 20:15:08 +0100

    VS solution upgrade to 2015 and MSBuild 14

--- build.cmd
+++ build.cmd
@@ -29,11 +29,16 @@ popd
 goto :EOF
 
 :main
-set MSBUILD4EXE=%windir%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe
-if not exist "%MSBUILD4EXE%" (
-    echo The .NET Framework 4.0 does not appear to be installed on this
-    echo machine, which is required to build the solution.
+setlocal
+if "%PROCESSOR_ARCHITECTURE%"=="x86" set MSBUILD=%ProgramFiles%
+if defined ProgramFiles(x86) set MSBUILD=%ProgramFiles(x86)%
+set MSBUILD=%MSBUILD%\MSBuild\14.0\bin\msbuild
+if not exist "%MSBUILD%" (
+    echo Microsoft Build Tools 2015 does not appear to be installed on this
+    echo machine, which is required to build the solution. You can install
+    echo it from the URL below and then try building again:
+    echo https://www.microsoft.com/en-us/download/details.aspx?id=48159
     exit /b 1
 )
-for %%i in (debug release) do "%MSBUILD4EXE%" Elmah.sln /p:Configuration=%%i /v:m %* || exit /b 1
+for %%i in (debug release) do "%MSBUILD%" Elmah.sln /p:Configuration=%%i /v:m %* || exit /b 1
 goto :EOF
